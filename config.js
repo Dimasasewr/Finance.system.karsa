48d4ef465161e9d7bacbfd001102d0856712aa6b
Dimasasewr <136103453+Dimasasewr@users.noreply.github.com>
2026-09-16 22:21:51 +0700

    config.js

--- config.js
+++ config.js
@@ -1,2 +1,7 @@
 const SUPABASE_URL = "https://vzkxtykuimmajrowxfey.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_abQ8dBWXzIIK1QQSX4tViw_ROS_KZlS";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
